9696211bf80f8f35726cca3c07cbb70e1ee455ed
KROUD-PC\carlo <carlos.corbano@hotmail.com>
2017-05-24 16:29:38 -0300

    S4A162 - How to use Blueprints Variables

--- BattleTank/Source/BattleTank/TankPlayerController.h
+++ BattleTank/Source/BattleTank/TankPlayerController.h
@@ -15,10 +15,12 @@ class BATTLETANK_API ATankPlayerController : public APlayerController
 	GENERATED_BODY()
 	
 public:
-	ATank* GetControlledTank() const;
 	virtual void Tick(float DeltaTime) override;
 
 protected:
+	UFUNCTION(BlueprintCallable, Category = "Setup")
+	ATank* GetControlledTank() const;
+
 	virtual void BeginPlay() override;
 
 private:
